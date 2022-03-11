a7aaa3977ba94ab7f6d6e6ec0ee825f4b197e034
Raiden DeKoter <raiden.dekoter@gmail.com>
2022-03-10 20:06:34 -0600

    initial working solution

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+function astronautDataToHTML(astronautDataJSON) {
+    return `
+<div class="astronaut">
+    <div class="bio">
+        <h3>${astronautDataJSON.firstName} ${astronautDataJSON.lastName}</h3>
+        <ul>
+            <li>Hours in space: ${astronautDataJSON.hoursInSpace}</li>
+            <li>Active: ${astronautDataJSON.active}</li>
+            <li>Skills: ${astronautDataJSON.skills.join(', ')}</li>
+        </ul>
+    </div>
+    <img class="avatar" src="${astronautDataJSON.picture}">
+</div>`
+}
+
+window.addEventListener('load', () => {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(response => {
+        response.json().then(json => {
+            const container = document.getElementById('container');
+            for (const astronaut of json) {
+                container.innerHTML += astronautDataToHTML(astronaut);
+            }
+        });
+    });
+});
